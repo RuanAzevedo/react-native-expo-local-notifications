1869ad6043b6163b410cccd4813d906814ac629f
Ruan Azevedo <ruan.azevedo@gmail.com>
2023-12-27 15:18:06 -0500

    Add notification received/response-received listeners

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,14 @@
 import { Button, StyleSheet } from 'react-native';
 
 import { View } from '../../components/Themed';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 import * as Notifications from 'expo-notifications';
 
 export default function TabOneScreen() {
+  const notififReceivedListener = useRef<Notifications.Subscription>();
+  const notifResponseListener = useRef<Notifications.Subscription>();
+
   useEffect(() => {
     Notifications.requestPermissionsAsync({
       ios: {
@@ -17,6 +20,39 @@ export default function TabOneScreen() {
     }).then((status) => {
       console.log('PERMISSIONS:', JSON.stringify(status, null, 2));
     });
+
+    /**
+     * This listener is fired whenever a notification is received while the app is foregrounded
+     */
+    notififReceivedListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        console.log(
+          'NOTIFICATION RECEIVED:',
+          JSON.stringify(notification, null, 2)
+        );
+      });
+
+    /**
+     * This listener is fired whenever a user taps on or interacts with a notification
+     * (works when app is foregrounded, backgrounded, or killed)
+     */
+    notifResponseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(
+          'NOTIFICATION RESPONSE:',
+          JSON.stringify(response, null, 2)
+        );
+        alert(response.notification.request.content.data.data);
+      });
+
+    return () => {
+      Notifications.removeNotificationSubscription(
+        notififReceivedListener.current!
+      );
+      Notifications.removeNotificationSubscription(
+        notifResponseListener.current!
+      );
+    };
   }, []);
 
   const scheduleNotification = async () => {
@@ -24,7 +60,7 @@ export default function TabOneScreen() {
       content: {
         title: "You've got mail! 📬",
         body: 'Here is the notification body',
-        data: { data: 'goes here' },
+        data: { data: 'notification data' },
       },
       trigger: { seconds: 5 },
     });
